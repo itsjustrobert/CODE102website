1dd0577afa4a36060f29cdc2d590116fe467dfbf
itsjustrobert <robertlove99999@gmail.com>
2021-07-01 05:32:06 -0500

    added some js

---
+++ INDEX/javasript/home.js
@@ -0,0 +1,9 @@
+
+var name = prompt("What is your name?");
+console.log(name)
+var user_age = prompt("How old are you " + name);
+console.log(user_age);
+if(user_age >= 18){prompt("Please enter a unique username")}
+else if(user_age < 18){alert("PLEASE LEAVE NOW!!")}
+prompt("Please enter a  password " + name)
+
