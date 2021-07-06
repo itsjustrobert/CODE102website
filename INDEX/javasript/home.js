84e23248261947a3e7c8f6de6131ccd826b60058
itsjustrobert <robertlove99999@gmail.com>
2021-07-06 10:21:45 -0500

    more changes

--- INDEX/javasript/home.js
+++ INDEX/javasript/home.js
@@ -1,9 +1,17 @@
+function askQuestion(){
+    var numberfound = false;
+    while(numberfound ==false){
+        var number = prompt("enter a number 1 -10");
+        if(number == "7"){
+            alert("That's Correct");
+            numberfound = true;
+        }
+        else{
+            alert("INCORRECT!");
+        }
+    
+    }
+}    
 
-var name = prompt("What is your name?");
-console.log(name)
-var user_age = prompt("How old are you " + name);
-console.log(user_age);
-if(user_age >= 18){prompt("Please enter a unique username")}
-else if(user_age < 18){alert("PLEASE LEAVE NOW!!")}
-prompt("Please enter a  password " + name)
+askQuestion();
 
